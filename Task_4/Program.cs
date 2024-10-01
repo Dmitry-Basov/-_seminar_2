8b8f3d6fd56a4644a6fb3fe27cd6520026f0f26f
Dmitriy Basov <dmitrybasov_787788@mail.ru>
2024-10-01 04:47:39 +0300

    seminar2:Task4

---
+++ Task_4/Program.cs
@@ -0,0 +1,22 @@
+﻿// Напишите программу, которая выводит третью слева
+// цифру заданного числа или сообщает, что третьей
+// цифры нет.
+
+
+
+Console.WriteLine("Введите число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+if (num >= 100 || num <= -100)
+{
+    while (num > 999 || num < -999)
+    {
+        num = num / 10;
+    }
+    int result = num % 10;
+    Console.WriteLine("третья цифра " + result);
+}
+else
+{
+    Console.WriteLine("третьей цифры нет");
+}
