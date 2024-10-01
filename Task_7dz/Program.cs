7bcd32aae47c568adebeb55ecfd38a433039ac9f
Dmitriy Basov <dmitrybasov_787788@mail.ru>
2024-10-01 23:48:58 +0300

    seminar2:Task_7dz

---
+++ Task_7dz/Program.cs
@@ -0,0 +1,25 @@
+﻿// Напишите программу, которая принимает на вход целое число 
+// из отрезка [10, 99] и показывает наибольшую цифру числа.
+
+
+
+Console.WriteLine("Введите двухзначное число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+if (num >= 10 && num <= 99)
+{
+    int firstnumber = num / 10;
+    int secondnumber = num % 10;
+    if (firstnumber < secondnumber)
+    {
+        Console.WriteLine(secondnumber);
+    }
+    else
+    {
+        Console.WriteLine(firstnumber);
+    }
+}
+else
+{
+    Console.WriteLine("Некорректные данные");
+}
